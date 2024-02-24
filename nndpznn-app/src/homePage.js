3560fe4d2f06f220319065efd0044500be610e8b
Nolan Nguyen! <nolan2087@gmail.com>
2024-02-24 14:28:31 -0800

    rearranging site files and implementing home page for react app

---
+++ nndpznn-app/src/homePage.js
@@ -0,0 +1,29 @@
+import "./style.css";
+
+function HomePage() {
+  return (
+    <div id="top-of-page">
+      <h1>
+        <b>
+          Hello, welcome to Nolan Nguyen's personal site! This is a
+          work-in-progress.
+        </b>
+      </h1>
+      <img
+        src="assets/sitepfp2.JPG"
+        class="imagecenter scaled roundedimage"
+        alt="Nolan PFP 2"
+      />
+
+      <p class="textcenter">
+        I'm currently learning HTML and CSS to make it look pretty. Eventually,
+        it'll serve as a central hub for all of my Computer Science and creative
+        projects.
+        <br></br>
+        But, for the time being ... here it is.
+      </p>
+    </div>
+  );
+}
+
+export default HomePage;
